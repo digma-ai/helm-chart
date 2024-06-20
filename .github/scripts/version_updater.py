34d905da43a4d61336dbf58762ff9e8633ebd7e9
Shay Keren <shay.keren@gmail.com>
2024-06-20 02:29:06 +0200

    update version updater script

--- .github/scripts/version_updater.py
+++ .github/scripts/version_updater.py
@@ -1,5 +1,4 @@
-import yaml
-import sys
+from ruamel.yaml import YAML
 
 
 def increment_version(version, level):
@@ -27,9 +26,40 @@ def increment_version(version, level):
     return f"{major}.{minor}.{patch}"
 
 
+def update_deployment_version(version, chart_version, deployment_yml):
+    yml = YAML()
+    with open(deployment_yml, 'r') as file:
+        docs = list(yml.load_all(file))
+    excludes = ["digma-ds-deployment"]
+    for data in docs:
+        app_name = data['metadata']['name']
+        if data['kind'] != 'Deployment' or app_name in excludes:
+            continue
+
+        containers = data['spec']['template']['spec']['containers']
+        for container in containers:
+            image = container['image']
+            # Split the image string to get the version part
+            image_name = image.split(':')[0]
+            container['image'] = f'{image_name}:{version}'
+            app_version_dic = next(e for e in container['env'] if e['name'] == 'ApplicationVersion')
+            app_version_dic['value'] = version
+            chart_version_dic = next(e for e in container['env'] if e['name'] == 'ChartVersion')
+            chart_version_dic['value'] = chart_version
+
+    with open(deployment_yml, 'w') as file:
+        yml.dump_all(docs, file)
+
+
+def update(version, chart_file_path, deployment_yml):
+    chart_version = update_chart_version(version, chart_file_path)
+    update_deployment_version(version, chart_version, deployment_yml)
+
+
 def update_chart_version(version, chart_file_path):
+    yml = YAML()
     with open(chart_file_path, 'r') as file:
-        chart = yaml.safe_load(file)
+        chart = yml.load(file)
 
     chart['appVersion'] = version
 
@@ -42,7 +72,9 @@ def update_chart_version(version, chart_file_path):
     chart['version'] = new_version
 
     with open(chart_file_path, 'w') as file:
-        yaml.safe_dump(chart, file, default_flow_style=False)
+        yml.dump(chart, file)
+
+    return new_version
 
 
 if __name__ == "__main__":
@@ -54,5 +86,5 @@ if __name__ == "__main__":
     chart_yml = sys.argv[2]
     deployment_yml = sys.argv[3]
     
+    update(new_version, chart_yml, deployment_yml)
 
-    update_chart_version(new_version, chart_yml)
